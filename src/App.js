ca74ae796c35c7172e22447151f1bd0aa1a45e51
Yuguo Zhao <>
2021-01-04 11:58:44 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+import BookShelves from './BookShelves';
+
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false,
+    bookList: [],
+    //bookStatus: "none",
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(data=>{
+      console.log(data);
+      this.setState((preState)=>({
+        bookList: preState.bookList.concat(data),
+      }))
+    })
+  }
+
+  updateBookStatus = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+    .then(status=>{
+      console.log(status)
+      this.setState((preState)=>({
+        bookList: preState.bookList.concat(status),
+      }))
+    })
+
+  }
+
+
+  render() {
+
+    const { bookList } = this.state;
+
+    return (
+      <div className="app">
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+              <BookShelves
+                bookList={bookList}
+                updateBookStatus={this.updateBookStatus}
+              />
+            <div className="open-search">
+              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default BooksApp
---
+++ src/Category.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+//import * as BooksAPI from './BooksAPI';
+
+class Category extends Component {
+
+  state = {
+    shelfValue: "none",
+  }
+
+  updateBookShelf = (e) => {
+    const bookObj = {id: this.props.book.id};
+    console.log(bookObj)
+    const shelf = e.target.value;
+
+    this.setState((preState)=>({
+      shelfValue: shelf,
+    }))
+    if (this.props.updateBookStatus) {
+    this.props.updateBookStatus(bookObj, this.state.shelfValue);
+    }
+  }
+
+
+  render() {
+    const { book } = this.props;
+    const { shelfValue } = this.state;
+
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks.thumbnail})`
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select value={shelfValue} onChange={this.updateBookShelf}>
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Category;
+/*
+<li>
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+      <div className="book-shelf-changer">
+        <select>
+          <option value="move" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">Ender's Game</div>
+    <div className="book-authors">Orson Scott Card</div>
+  </div>
+</li>
+*/
